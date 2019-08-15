e8650e7a4972527562c8e36f7497bfe30b7b4e79
Marcio Schneider <29129082+marciorschneider@users.noreply.github.com>
2019-08-15 12:40:16 -0300

    new release

---
+++ OpenLDAPStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("CustomLapStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomLapStore")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab57750a-5d4c-4a2f-b8d2-dcf76d06a228")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.40")]
---
+++ TestLdapStore/TestLDAP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenLDAPStore;
+
+namespace TestLdapStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LdapAnonymousStore attributeStore = new LdapAnonymousStore();
+            Dictionary<string, string> config = new Dictionary<string, string>();
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Enter a filter and a parameter.");
+            }
+            else
+            {
+                string filter = args[0];
+                string parameters = args[1];
+
+                config.Add("host", "openldap-slave.sicredi.net");
+                config.Add("base", "dc=sicredi,dc=com,dc=br");
+
+                attributeStore.Initialize(config);
+                IAsyncResult result = attributeStore.BeginExecuteQuery(filter, new string[] { parameters }, null, null);
+             
+                string[][] data = attributeStore.EndExecuteQuery(result);
+                foreach (string[] row in data)
+                {
+                    foreach (string col in row)
+                    {
+                        Console.Write("{0}\n", col);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
+
